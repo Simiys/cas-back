edfe6ee072bf3032481f1c6773e05d7fe6a7500d
Simiys <mr.simiys@gmail.com>
2025-10-19 07:21:59 +0300

    shared_models fixes

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from pydantic import BaseSettings
+from pydantic_settings  import BaseSettings
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 load_dotenv(os.path.join(BASE_DIR, ".env"))
