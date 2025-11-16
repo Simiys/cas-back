7b2becc9d935ffb92b47330c52813417f99c0918
Simiys <mr.simiys@gmail.com>
2025-11-16 18:39:40 +0300

    deploy fix

--- shared_models/db.py
+++ shared_models/db.py
@@ -10,7 +10,7 @@ import shared_models.models
 DB_USER = os.getenv("POSTGRES_USER")
 DB_PASS = os.getenv("POSTGRES_PASSWORD")
 DB_HOST = os.getenv("POSTGRES_HOST")
-DB_PORT = os.getenv("POSTGRES_PORT")
+DB_PORT = int(os.getenv("POSTGRES_PORT", 5432))
 DB_NAME = os.getenv("POSTGRES_DB")
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
