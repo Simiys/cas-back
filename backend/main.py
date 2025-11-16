ad5fb9697204bdb06fe8f67750ff10268df176d1
Simiys <mr.simiys@gmail.com>
2025-11-16 18:36:34 +0300

    deploy fix

--- backend/main.py
+++ backend/main.py
@@ -79,4 +79,4 @@ async def ping():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
