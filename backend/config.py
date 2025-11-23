581ebf97d7a05d7a9e11541c90034bdebeb4a2fc
Simiys <mr.simiys@gmail.com>
2025-11-24 01:49:08 +0300

    cfg fix

--- backend/config.py
+++ backend/config.py
@@ -1,11 +1,18 @@
 import os
 from dotenv import load_dotenv
-from pydantic_settings  import BaseSettings
+from pydantic_settings  import BaseSettings, SettingsConfigDict
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 class Settings(BaseSettings):
+
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: str
+    POSTGRES_DB: str
+
     SECRET_KEY: str
     REDIS_URL: str | None = None
     TOKEN_EXPIRE_MINUTES: int = 60*24*7  # 7 дней по умолчанию
@@ -16,9 +23,11 @@ class Settings(BaseSettings):
     MNEMONIC: str
     TONCENTER_API_KEY: str
 
-    class Config:
-        env_file = os.path.join(BASE_DIR, ".env")
-        env_file_encoding = "utf-8"
+    model_config = SettingsConfigDict(
+        env_file=os.path.join(BASE_DIR, ".env"),
+        env_file_encoding="utf-8",
+        extra="ignore"  # игнорируем лишние переменные в окружении
+    )
 
 settings = Settings()
 
